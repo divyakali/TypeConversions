692f6768cdfb2e10230924d62ac280317156aba8
aethena <divyakali1288@gmail.com>
2015-07-22 09:41:25 -0400

    documented conditions in conversion logic

--- src/com/typeconversions/StringConversions.java
+++ src/com/typeconversions/StringConversions.java
@@ -1,12 +1,15 @@
 package com.typeconversions;
+
 /**
  * Implements type conversions
+ * 
  * @author aethena
  *
  */
 public class StringConversions {
 	/**
 	 * Convert string to long
+	 * 
 	 * @param str
 	 * @return
 	 */
@@ -16,7 +19,9 @@ public class StringConversions {
 
 		long i = 0;
 		char[] letters = str.toCharArray();
+		// check if number is negative and parse
 		if (str.startsWith("-")) {
+			// if number has more than 20 characters(includes -)
 			if (str.length() > 20)
 				throw new NumberFormatException(
 						"Number to convert is too small");
@@ -24,11 +29,15 @@ public class StringConversions {
 				letters = str.substring(1).toCharArray();
 		}
 		if (!str.matches("[0-9]{1,19}")) {
+			// if number has more than 19 characters
 			if (str.length() > 20)
 				throw new NumberFormatException("Number to convert is too big");
 		}
 
 		for (char ch : letters) {
+			// check if adding the new digit will create overflow/underflow
+			// problems. First condition is for positive numbers and second is
+			// for negative
 			if ((Long.MAX_VALUE - (i * 10)) >= Character.getNumericValue(ch)
 					|| (str.startsWith("-") && (Long.MIN_VALUE + (i * 10) <= -Character
 							.getNumericValue(ch))))
@@ -43,7 +52,5 @@ public class StringConversions {
 			return i;
 
 	}
-	
-
 
 }
